f462333dac3f5116a4113e68a9961400fecaa06f
Shawn Thompson <plansmash@me.com>
2025-08-30 18:07:03 -0400

    Add comprehensive HTML cleaning system for empty p tags
    - Create post-build script to remove empty p elements from all HTML files
    - Add Eleventy transform to clean empty p tags during development
    - Update package.json to run HTML cleaning after build
    - Handle various empty p tag patterns including those wrapping divs
    - Iterative cleaning to catch nested/multiple empty p tags
    - Improve Bootstrap grid compatibility by preventing Markdown interference

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,49 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
+  // Add HTML transform to clean up empty p tags during development
+  eleventyConfig.addTransform("cleanEmptyParagraphs", function(content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      // Remove various forms of empty p tags with multiple passes
+      let cleaned = content;
+      let previousContent;
+      
+      // Keep cleaning until no more changes
+      do {
+        previousContent = cleaned;
+        cleaned = cleaned
+          // Basic empty p tags
+          .replace(/<p>\s*<\/p>/g, '')
+          .replace(/<p[^>]*>\s*<\/p>/g, '')
+          .replace(/<p><\/p>/g, '')
+          // Empty p tags with non-breaking spaces
+          .replace(/<p>\s*&nbsp;\s*<\/p>/g, '')
+          .replace(/<p[^>]*>\s*&nbsp;\s*<\/p>/g, '')
+          // P tags incorrectly wrapping div elements (CRITICAL FIX)
+          .replace(/<p>\s*(<div[^>]*>)/g, '$1')
+          .replace(/(<\/div>)\s*<\/p>/g, '$1')
+          // P tags between elements (common Markdown issue)
+          .replace(/><p><\/p></g, '>')
+          .replace(/><p>\s*<\/p></g, '>')
+          .replace(/><p><\/p>\n/g, '>\n')
+          .replace(/><p>\s*<\/p>\n/g, '>\n')
+          // P tags at line boundaries
+          .replace(/\n<p><\/p>/g, '')
+          .replace(/\n<p>\s*<\/p>/g, '')
+          .replace(/<p><\/p>\n/g, '')
+          .replace(/<p>\s*<\/p>\n/g, '')
+          // Multiple consecutive p tags
+          .replace(/(<p><\/p>)+/g, '')
+          .replace(/(<p>\s*<\/p>)+/g, '')
+          // Clean up extra whitespace
+          .replace(/\n\s*\n\s*\n/g, '\n\n');
+      } while (cleaned !== previousContent);
+      
+      return cleaned;
+    }
+    return content;
+  });
+  
   // Add navigation plugin
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   
